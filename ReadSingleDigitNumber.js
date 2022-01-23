6e41410e2cea391841b10335b64a80a5b76c0eaa
rksgr <r_kjha@yahoo.com>
2022-01-23 13:44:02 +0530

    Program to read a single digit number and write the number in word

---
+++ ReadSingleDigitNumber.js
@@ -0,0 +1,27 @@
+// Read a number and display the number in word
+
+const prompt = require("prompt-sync")();
+
+let num = prompt("Enter a single digit number: ");
+
+// Display the number in word
+if (num == 1){
+    console.log("one");
+}else if(num == 2){
+    console.log("Two");
+}else if(num == 3){
+    console.log("Three");
+}else if(num == 4){
+    console.log("Four");
+}else if(num == 5){
+    console.log("Five");
+}else if(num == 6){
+    console.log("Six");
+}else if(num == 7){
+    console.log("Seven");
+}else if(num == 8){
+    console.log("Eight");
+}else if(num == 9){
+    console.log("Nine");
+}
+    
